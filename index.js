1280505ab7c41f3ef3455b47b2663ea6f952ac06
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 17:01:11 +0200

    taskJS-10

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
+// Дана также кнопка. По клику на кнопку выведите толщину границы, ее тип и цвет.
 // Рішення:
 document.addEventListener('click', function () {
 	let elem = document.querySelector('div');
-	elem.style.width = parseInt(elem.style.width) +parseInt(elem.style.width)/10 + 'px';
-	elem.style.height = parseInt(elem.style.height) +parseInt(elem.style.height)/10+ 'px';
-	console.log(elem.style.height,elem.style.width)
+	console.log(elem.style.border)
 })
