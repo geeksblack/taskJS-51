658d8cb2bec6514bea9cb286841484050502677d
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 15:54:14 +0200

    taskJS-3

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 // Дана также кнопка. По клику на кнопку выведите ширину и высоту дива без единиц измерения.
-document.addEventListener('click', function () {
-    document.getElementById("elem").style.width = "400px";
-    document.getElementById("elem").style.height = "300px";
-    document.getElementById("elem").style.border = "1px solid black";
-})
 
+
+
+document.addEventListener('click', function () {
+    let elem = document.getElementById('elem');
+    let style = getComputedStyle(elem);
+    console.log('width: '+parseInt(style.width) +', height: '+parseInt(style.height));
+})
 
