2f8540c0e13782b4d2aeb66f313eecc80532a38f
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 17:33:09 +0200

    taskJS-11

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// Дана также кнопка. По клику на кнопку выведите толщину границы, ее тип и цвет.
+// Дан див и две кнопки. По клику на первую кнопку спрячьте див, а по клику на вторую - покажите.
+
 // Рішення:
-document.addEventListener('click', function () {
+document.addEventListener('click', hide())
+function hide() {
+let elem = document.querySelector('div');
+	let hide = elem.style.display = 'none';
+	console.log(hide);	
+}
+document.addEventListener('click', show())
+function show() {
 	let elem = document.querySelector('div');
-	console.log(elem.style.border)
-})
+	let show = elem.style.display = 'block';
+	console.log(show);
+}
