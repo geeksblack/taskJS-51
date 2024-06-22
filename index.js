0d962d115bc3d323fcab7e973c627aabb1f79591
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 18:54:47 +0200

    taskJS-18

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Сделайте класс Employee.
 // Создайте объект класса Employee и выведите его в консоль.
 class Employee{
+	name;
+	age;
+	salary;
 }
 let employee = new Employee;
-console.log(employee);
+employee.name = 'Yuriy';
+employee.age = 22;
+employee.salary='2000$'
+console.log(employee.name);
+console.log(employee.age);
+console.log(employee.salary);
