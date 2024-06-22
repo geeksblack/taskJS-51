3c7d5c9c40639d551cec0074639cd3c329693cc0
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 16:27:12 +0200

    taskJS-7

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Дана также кнопка. По клику на кнопку выведите размер шрифта дива без единиц измерения.
 // Объясните, в чем ошибка в следующем коде:
-
 let elem = document.querySelector('div');
-elem.style.fontSize = parseInt(elem.style.fontSize + 2) + 'px';
+elem.style.fontSize = parseInt(elem.style.fontSize) + 2 + 'em';
 
 // Рішення:
 let elem = document.querySelector('div');
-elem.style.fontSize = parseInt(elem.style.fontSize ) + 2 + 'px';
+elem.style.fontSize = parseFloat(elem.style.fontSize) + 2 + 'em';
 console.log(elem.style.fontSize)
