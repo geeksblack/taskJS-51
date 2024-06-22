5be697def0348e422effbeee5dcf396c64b40c06
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 19:37:49 +0200

    taskJS-25

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 /*
-№1⊗jsOpBsMIC
-
-В объект класса Student запишите свойства name и surn.
-
-№2⊗jsOpBsMIC
-
-Сделайте вспомогательный метод, который будет получать первый символ строки и делать его заглавным.
-
-№3⊗jsOpBsMIC
-
-Сделайте метод, который вернет инициалы студента, то есть первые буквы его имени и фамилии.
-
-
-
-
-
-
+Сделайте конструктор класса Employee.
 */
-class Student {
-	name='Yuriy';
-	surn = 'Vysotskyi';
-	showName() {
-		return this.name;
-	}
-    showSurn() {
-		return this.surn;
+class Employee {
+	constructor() {
+		console.log('test');
 	}
 
 }
-let student = new Student;
-console.log(student.showName())
-console.log(student.showSurn())
+new Employee;
+
