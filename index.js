93cd7197a0b8c4112ee8a0aa1609180d98615339
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 15:59:07 +0200

    taskJS-3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Дана также кнопка. По клику на кнопку выведите ширину и высоту дива без единиц измерения.
+// Дана также кнопка. По клику на кнопку выведите размер шрифта дива без единиц измерения.
 
 
 
 document.addEventListener('click', function () {
     let elem = document.getElementById('elem');
     let style = getComputedStyle(elem);
-    console.log('width: '+parseInt(style.width) +', height: '+parseInt(style.height));
+    console.log('font-size: '+parseFloat(style.fontSize));
 })
 
