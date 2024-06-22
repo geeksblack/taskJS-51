bb80eccc14a6aebf2ce6c94ffdf01bb9250ae686
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 16:57:07 +0200

    taskJS-9

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Рішення:
 document.addEventListener('click', function () {
 	let elem = document.querySelector('div');
-	elem.style.width = parseInt(elem.style.width) + 50 + 'px';
-	elem.style.height = parseInt(elem.style.height) + 50 + 'px';
+	elem.style.width = parseInt(elem.style.width) +parseInt(elem.style.width)/10 + 'px';
+	elem.style.height = parseInt(elem.style.height) +parseInt(elem.style.height)/10+ 'px';
 	console.log(elem.style.height,elem.style.width)
 })
