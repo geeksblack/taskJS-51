2f0022cacbd1f43993ed566e62f6c15d02a2a9d8
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 15:24:19 +0200

    taskJS-2

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-let elem = document.querySelector('#elem');
-elem.style.color = 'red';
-console.log(elem.style.color);
+
+// Дана также кнопка. По клику на кнопку выведите ширину и высоту дива без единиц измерения.
+document.addEventListener('click', function () {
+    document.getElementById("elem").style.width = "400px";
+    document.getElementById("elem").style.height = "300px";
+    document.getElementById("elem").style.border = "1px solid black";
+})
+
+
