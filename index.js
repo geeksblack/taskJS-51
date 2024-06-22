a25d6f54abfc72d90997a358c548f40d128da1e5
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 19:03:44 +0200

    taskJS-19

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-// Сделайте класс Employee.
-// Создайте объект класса Employee и выведите его в консоль.
+// Выведите на экран сумму зарплат созданных вами работников.
+// Запишите в свойство каждого работника его имя и зарплату.
+// Создайте несколько объектов класса Employee.
+
+
+
 class Employee{
 	name;
 	age;
 	salary;
 }
-let employee = new Employee;
-employee.name = 'Yuriy';
-employee.age = 22;
-employee.salary='2000$'
-console.log(employee.name);
-console.log(employee.age);
-console.log(employee.salary);
+let employee1 = new Employee;
+let employee2 = new Employee;
+let employee3 = new Employee;
+employee1.name = 'Yuriy';
+employee1.salary = '2000';
+employee2.name = 'Max';
+employee2.salary = '1500';
+employee3.name = 'Igor';
+employee3.salary = '1900';
+console.log('зарплата 1 робітника : ' + employee1.name+' '+employee1.salary);
+console.log('зарплата 2 робітника : ' + employee2.name + ' ' + employee2.salary);
+console.log('зарплата 3 робітника : ' + employee3.name + ' ' + employee3.salary);
+let sum = parseInt(employee1.salary) + parseInt(employee2.salary) + parseInt(employee3.salary);
+console.log('сума зарплат робітників : ' +sum) ;
