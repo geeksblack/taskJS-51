77ce4109468c4bd648a60c5313b36d66c7a3466b
yuriyspacee <yuriizspace@gmail.com>
2024-06-22 16:24:00 +0200

    taskJS-6

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 // Дана также кнопка. По клику на кнопку выведите размер шрифта дива без единиц измерения.
 // Объясните, в чем ошибка в следующем коде:
 
-<div style="font-size: 16px;">
-	text
-</div>
 let elem = document.querySelector('div');
-elem.style.fontSize = elem.style.fontSize + 2;
-
-
+elem.style.fontSize = parseInt(elem.style.fontSize + 2) + 'px';
 
 // Рішення:
 let elem = document.querySelector('div');
-let fontSize = elem.style.fontSize;
-let changefontSize = parseInt(fontSize);
-elem.style.fontSize = (changefontSize + 2) + "px";
-console.log(elem.style.fontSize);
+elem.style.fontSize = parseInt(elem.style.fontSize ) + 2 + 'px';
+console.log(elem.style.fontSize)
