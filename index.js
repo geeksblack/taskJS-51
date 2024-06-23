5c4f9115ffb154909fc5cff0215cc444494601b4
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 14:53:53 +0200

    taskJS-47

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 /*
-В классе Employee исправьте упростите сеттер возраста, использовав оригинальный метод родителя.
+Пусть класс Employee наследует от класса User из данного урока.
+
+№2⊗jsOpIhCn
+
+Переопределите в класс Employee конструктор родителя, добавив в него параметр с возрастом и зарплатой.
+
+№3⊗jsOpIhCn
+
+Сделайте геттеры для возраста и зарплаты.
+
+
 */
-class User {
-	setAge(age) {
-		if (age >= 0) {
-			this.age = age;
-		} else {
-			throw new Error('need age more 0');
-		}
+class User{
+	constructor(name, surn) {
+		this.name = name;
+		this.surn = surn;
+	}
+	getName() {
+		return this.name;
+	}
+	getSurn() {
+		return this.surn;
 	}
 }
-class Employee {
-	setAge(age) {
-		if (age <= 120) {
-		super.setAge(age);
-		} else {
-			throw new Error('need age less 120');
-		}
+class Employee{
+	constructor(name, surn, salary, age) {
+		super(name, surn);
+		this.salary = salary;
+		this.age = age;
+	}
+	getAge() {
+		return this.age;
+	}	
+	getSalary() {
+		return this.salary;
 	}
 }
