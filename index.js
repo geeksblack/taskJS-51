8c6234589d880acde1c68589e9f99c066d8b3df2
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 14:16:38 +0200

    taskJS-42

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// Определите, какому классу принадлежит данный тег.
-// let elem = document.querySelector('div');
-// console.dir(elem instanceof HTMLDivElement);
+// Сделайте класс Employee, наследующий от класса User
+class User{
 
-// let elems = document.querySelectorAll('div');
-// console.dir(elems instanceof NodeList);
+}
+class Employee extends User{
 
-// let elem = document.querySelector('input');
-// console.dir(elem instanceof HTMLInputElement
-// );
-
-let elem = document.querySelector('div');
-let elems = elem.children;
-
-console.dir(elems instanceof HTMLCollection
-);
+}
