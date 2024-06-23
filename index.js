dddf952c553127b10619505638df56a1f4436d95
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 14:23:03 +0200

    taskJS-43

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// Сделайте класс Employee, наследующий от класса User
+// Проверьте, что ваш класс Employee наследует методы от класса User.
 class User{
-
+	setName(name) {
+		this.name = name;
+	}
+	getName() {
+		return this.name;
+	}
 }
 class Employee extends User{
 
 }
+let employee = new Employee;
+employee.setName('Yuriy');
+let name = employee.getName();
+console.log(name);
