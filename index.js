9350ea57d432517123b9aa9b374c8063177aca0f
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 15:07:36 +0200

    taskJS-49

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 /*
-Попробуйте в классе Employee воспользоваться приватным методом родителя.
+№1⊗jsOpIhPrP
 
+Дан следующий класс-родитель:
 
-
-*/
-
-class User{
+class User {
+	#name;
+	#surn;
+	
 	setName(name) {
-		this.name = name;
+		this.#name = name;
 	}
 	getName() {
-		return this.name;
+		return this.#name;
 	}
-	#capeFirst(str) {
-		return str[0].toUpperCase() + str.splice(1);
+	
+	setSurn(surn) {
+		this.#surn = surn;
+	}
+	getSurn() {
+		return this.#surn;
+	}
+}
+Сделайте класс Employee, который будет наследовать от этого родителя.
+
+№2⊗jsOpIhPrP
+
+Создайте объект класса Employee и вызовите унаследованные сеттеры и геттеры.
+
+№3⊗jsOpIhPrP
+
+В классе Employee сделайте следующий метод:
+
+getFull() {
+	return this.#name + ' ' + this.#surn;
+}
+Убедитесь, что код метода приведет к ошибке.
+
+*/
+class User {
+	#name;
+	#surn;
+	
+	setName(name) {
+		this.#name = name;
+	}
+	getName() {
+		return this.#name;
+	}
+	
+	setSurn(surn) {
+		this.#surn = surn;
+	}
+	getSurn() {
+		return this.#surn;
 	}
 }
 class Employee extends User{
-	setSurn(surn) {
-		this.surn = surn;
-	}
-	getSurn() {
-		return this.#capeFirst(this.surn);//
-	}
+getFull() {
+	return this.#name + ' ' + this.#surn;
 }
+}
+let employee = new Employee;
+employee.setName('Yuriy');
+employee.setSurn('Vysotskyi');
+let name = employee.getName();
+let surn = employee.getSurn();
+console.log(name, surn);
