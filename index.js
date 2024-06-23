e6eb86b17b9ec617368fb3b78367776812f3c37c
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 11:46:49 +0200

    taskJS-36

--- index.js
+++ index.js
@@ -1,47 +1,83 @@
 /*
-Сделайте так, чтобы сеттеры класса Employee можно было вызывать цепочкой.
-
+Расскажите, каким будет результат сравнения в следующем коде:
 
 
 */
-class Employee{
-	#name;
-	#age;
-	#salary;
-	setName(name) {
-		this.#name = name;
-		return this;	
-	}
-	setAge(age) {
-		if (age > 0 && age < 120) {
-			this.#age = age;
-			return this;
-		} else {
-			throw new Error('age is incorrect');
-		}
-			
-	}
-	setSalary(salary) {
-		this.#salary = salary;
-		return this;
-		
-	}
-	getName() {
-		return this.#name;
-	}
-	getAge() {
-		return this.#age;
-	}
-	getSalary() {
-		return this.addSign(this.#salary);
-	}
-    addSign(num) {
-		return num + '$';
-	}
-}
-let employee = new Employee;
-employee.setName('Yuriy').setAge(22).setSalary('2000');
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
 
-console.log(employee.getName());
-console.log(employee.getAge());
-console.log(employee.getSalary());
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 === emp2);
+// результат зрівняння буде false адже ми отримуємо ссилку на два різних обєкти
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 === emp1);
+// результат зрівняння буде false адже ми отримуємо ссилку на то й же обєкт
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('john');
+
+// console.log(emp1 === emp2);
+// результат зрівняння буде false адже ми отримуємо ссилку на два різних обєкти
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 !== emp1);
+// результат зрівняння буде false адже ми зрівнюємо з протилежним знаком
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = emp1;
+
+// console.log(emp1 === emp2);
+// результат буде true тому що ми переписали дану зміну;
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = new Employee('eric');
+
+// console.log(emp1 !== emp2);
+// результат буде true тому що ми заперечуємо рівність даних змін;
+// class Employee {
+// 	constructor(name) {
+// 		this.name = name;
+// 	}
+// }
+
+// let emp1 = new Employee('john');
+// let emp2 = emp1;
+// emp2.name = 'eric';
+
+// console.log(emp1 === emp2);
+//  результат буде true тому що ми зрівнюємо рівність даних змін;
