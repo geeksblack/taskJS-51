8d3c697519faa69a60ce700ab56f4118bbbdbb9c
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 11:01:49 +0200

    taskJS-32

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-// В следующем коде сделайте вспомогательный метод приватным:
+/*
+№1⊗jsOpBsPG
+
+В классе Employee сделайте три приватных свойства: имя, зарплату и возраст.
+
+№2⊗jsOpBsPG
+
+Передайте параметром конструктора значения этих свойств.
+
+№3⊗jsOpBsPG
+
+Сделайте геттеры, выводящие значения каждого из наших свойств.
 
 
-class Employee {
-	constructor(name, salary) {
-		this.name = name;
-		this.salary = salary;
-		this.#addSign();
+*/
+class Employee{
+	#name;
+	#age;
+	#salary;
+	constructor(name,age,salary) {
+		this.#name = name;
+		this.#age = age;
+		this.#salary = salary;
 	}
-	
+	getName() {
+		return this.#name;
+	}
+	getAge() {
+		return this.#age;
+	}
 	getSalary() {
-		return this.#addSign(this.salary);
+		return this.#salary;
 	}
-	
-	#addSign(num) {
-		return num + '$';
-	}
-}	
-let employee = new Employee('Max', '1500');
+
+}
+let employee = new Employee('Yuriy',22,'2000$');
+console.log(employee.getName());
+console.log(employee.getAge());
 console.log(employee.getSalary());
