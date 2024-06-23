9425502196e83203ed08f94a054e5fb3e76d3c4b
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 10:42:37 +0200

    taskJS-29

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-/*
-Передайте в конструктор класса Employee имя и зарплату работника.
-№1⊗jsOpBsPP
-
-В классе Employee сделайте три приватных свойства: имя, зарплату и возраст.
-
-№2⊗jsOpBsPP
-
-Передайте параметром конструктора значения этих свойств.
-
-№3⊗jsOpBsPP
-
-Сделайте метод, который выведет данные работника.
-
-*/
 class Employee {
-	#name;
-	#age;
-	#salary;
-	constructor(name,age,salary) {
-		this.#name = name;
-		this.#age = age;
-		this.#salary = salary;
+	constructor(name, salary) {
+		this.name = name;
+		this.salary = salary;
+		this.#addSign();
 	}
-	showName() {
-		return this.#name;
+	
+	getSalary() {
+		return this.#addSign(this.salary);
 	}
-	showAge() {
-		return this.#age;
+	
+	#addSign(num) {
+		return num + '$';
 	}
-	showSalary() {
-		return this.#salary;
-	}
-
 }
-let employee = new Employee('Yuriy', 22, '2000$');
-console.log(employee.showName());
-console.log(employee.showAge());
-console.log(employee.showSalary());
+let employee = new Employee('Yuriy', '2000');
+console.log(employee.getSalary());
