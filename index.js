b6f389e80b5131f7156ddf86230a02cb0a88f710
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 14:41:59 +0200

    taskJS-46

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
 /*
-№1⊗jsOpIhOPM
-
-В классе Employee сделайте геттер и сеттер возраста.
-
-№2⊗jsOpIhOPM
-
-В классе Employee переопределите сеттер возраста и сделайте в нем проверку на то, что возраст от 18 до 65 лет.
-
+В классе Employee исправьте упростите сеттер возраста, использовав оригинальный метод родителя.
 */
-class User{
+class User {
 	setAge(age) {
-		this.age = age;
-	} 
-	getAge() {
-		return this.age;
-	}
-	setName(name) {
-		this.name = name;
-	}
-	getName() {
-		return this.name;
-	}
-}
-class Employee extends User{
-	setAge(age) {
-		if (age > 18 && age < 65) {
+		if (age >= 0) {
 			this.age = age;
 		} else {
-			throw new Error('age is incorrect');
+			throw new Error('need age more 0');
 		}
 	}
-	setSalary(salary) {
-		this.salary = salary;
-	}
-	getSalary() {
-		return this.salary;
+}
+class Employee {
+	setAge(age) {
+		if (age <= 120) {
+		super.setAge(age);
+		} else {
+			throw new Error('need age less 120');
+		}
 	}
 }
-let employee = new Employee;
-employee.setName('Yuriy');
-employee.setSalary('2000$');
-employee.setAge(22);
-let name = employee.getName();
-let salary = employee.getSalary();
-let age = employee.getAge();
-console.log(name, salary,age);
