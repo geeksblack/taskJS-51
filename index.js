d97cb53b36fe9f9a963abdb943ab7df228bb70de
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 14:28:15 +0200

    taskJS-44

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// Проверьте, что ваш класс Employee наследует методы от класса User.
+/*
+№1⊗jsOpIhOChM
+
+В классе Employee сделайте геттер и сеттер зарплаты.
+
+№2⊗jsOpIhOChM
+
+Проверьте, что в классе Employee работают как его родные методы, так и унаследованные.
+
+
+*/
 class User{
 	setName(name) {
 		this.name = name;
@@ -8,9 +18,16 @@ class User{
 	}
 }
 class Employee extends User{
-
+	setSalary(salary) {
+		this.salary = salary;
+	}
+	getSalary() {
+		return this.salary;
+	}
 }
 let employee = new Employee;
 employee.setName('Yuriy');
+employee.setSalary('2000$');
 let name = employee.getName();
-console.log(name);
+let salary = employee.getSalary();
+console.log(name, salary);
