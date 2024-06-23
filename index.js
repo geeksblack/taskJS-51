236ae1d954faf7d0c8a59525f7222705dc126e7a
yuriyspacee <yuriizspace@gmail.com>
2024-06-23 12:51:02 +0200

    taskJS-40

--- index.js
+++ index.js
@@ -1,52 +1,30 @@
 /*
-№1⊗jsOpBsOIC
-
-Дан следующий класс:
-
-class Employee {
-	constructor(name, position, department) {
-		this.name = name;
-		this.position = position;
-		this.department = department;
-	}
-}
-Сделайте так, чтобы во второй и третий параметры передавались объекты отдельных классов.
-
-№2⊗jsOpBsOIC
-
-Создайте объект с работником с помощью класса из предыдущей задачи.
-
-№3⊗jsOpBsOIC
-
-Выведите в консоль имя, должность и отдел для созданного работника.
+Перебирите созданный массив циклом и выведите на экран имена и зарплаты каждого работника.
 
 */
-class Position{
-	constructor(name) {
-		this.name = name;
-	}
-}
-class Department{
-	constructor(name) {
-		this.name = name;
-	}
-}
-class Name{
-	constructor(name) {
-		this.name = name;
-	}
-}
 class Employee {
-	constructor(name, position, department) {
-		this.name = name;
-		this.position = position;
-		this.department = department;
+	#name;
+	#salary;
+	constructor(name, salary) {
+		this.#name = name;
+		this.#salary = salary;
 	}
+	getName() {
+		return this.#name;
+	}
+	getSalary() {
+		return this.#salary;
+	}
+
 }
-let position = new Position('php developer');
-let name = new Name('Yuriy');
-let department = new Department('IT software');
-let employee = new Employee(name, position, department);
-console.log(employee.name.name);
-console.log(employee.position.name);
-console.log(employee.department.name);
+let employees = [
+	new Employee('Max', 1000),
+	new Employee('Yuriy',2000),
+	new Employee('Nazar', 3000),
+	new Employee('Andriy', 1500),
+	new Employee('Sergiy', 1300),
+]
+for (let employee of employees) {
+	console.log(employee.getName());
+	console.log(employee.getSalary());
+}
